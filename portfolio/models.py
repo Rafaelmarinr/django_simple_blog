00372c4eedcf8f8e98367426e13f79264e04ebeb
Rafaelmarinr <rafaelmarinantonior@gmail.com>
2024-01-23 20:37:03 -0400

    Setting admin

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,8 +2,9 @@ from django.db import models
 from django.db.models.fields import CharField, URLField
 from django.db.models.fields.files import ImageField
 
+
 class Project(models.Model):
-    title = CharField(max_lenght=100)
-    description = CharField(max_lenght=250)
+    title = CharField(max_length=100)
+    description = CharField(max_length=250)
     image = ImageField(upload_to='portfolio/imagens')
     url = URLField(blank=True)
