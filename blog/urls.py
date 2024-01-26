17beb56417a1544189a70343c69ae38974331f02
Rafaelmarinr <rafaelmarinantonior@gmail.com>
2024-01-26 07:03:23 -0400

    Styles, first parte

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', render_post, name='posts'),
-    path('/<int:post_id>', post_detail, name='post_detail'),
+    path('<int:post_id>', post_detail, name='post_detail'),
 ]
